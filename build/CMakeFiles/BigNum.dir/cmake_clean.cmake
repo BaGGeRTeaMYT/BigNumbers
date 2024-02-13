2f5231727fb632e97feab452e74976fad1db17be
Andrew <kidd0@localhost.localdomain>
2024-02-14 02:46:16 +0300

    Getting started

---
+++ build/CMakeFiles/BigNum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BigNum"
+  "BigNum.pdb"
+  "CMakeFiles/BigNum.dir/src/main.cpp.o"
+  "CMakeFiles/BigNum.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BigNum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MyLib.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MyLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
